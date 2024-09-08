d838153a0a7ef9b7fc408628997fc3b199610fa6
Caio Augusto <caioaugustobr0106@gmail.com>
2024-09-07 22:08:01 -0300

    Segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 
 @NgModule({
   declarations: [ // declaração dos componentes é aqui que referenciamos os componentes no modulos
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardRoxoComponent
   ],
   imports: [ // definirmos os modulos
     BrowserModule,
